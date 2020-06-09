d569e3b44a642fb2734f6ae0f81df3323a24607d
Ezekiel <zekejenyart@gmail.com>
2020-06-09 18:31:41 -0500

    added webpages

---
+++ portfolio/PREWORK_EE/Module - 6/New folder/javascript.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+    document.getElementById("button1").addEventListener("click", function(){document.getElementById("box").style.height = "50px"})
+    document.getElementById("button2").addEventListener("click", function(){document.getElementById("box").style.backgroundcolor = "blue"})
+    document.getElementById("button3").addEventListener("click", function(){document.getElementById("box").style.opacity = "0.9"})
+    document.getElementById("button4").addEventListener("click", function(){document.getElementById("box").style.height = "150px"};{style.backgroundcolor = "orange"}{ style.opacity = "1" })
+</script>
